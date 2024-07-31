d27a9259a0da5def79510f5bad8c9956bf1a7029
Venktesh809 <103634213+Venktesh809@users.noreply.github.com>
2024-07-31 21:36:31 +0530

    Update Find_EvenOdd_Digit_Given_No.java

--- numbers/Find_EvenOdd_Digit_Given_No.java
+++ numbers/Find_EvenOdd_Digit_Given_No.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class Find_EvenOdd_Digit_Given_No {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the no.");
 		int n = sc.nextInt();
