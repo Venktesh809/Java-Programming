58c1f03f645e766ddc0e860f27fd879997831bad
Venktesh809 <103634213+Venktesh809@users.noreply.github.com>
2024-07-31 21:37:59 +0530

    Update Perfect_No.java

--- numbers/Perfect_No.java
+++ numbers/Perfect_No.java
@@ -5,8 +5,7 @@ import java.util.Scanner;
 public class Perfect_No {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 		Scanner sc= new Scanner(System.in);
 		System.out.println("Enter the no. ");
 		int n = sc.nextInt();
@@ -15,7 +14,6 @@ public class Perfect_No {
 		for(int i=1; i<n; i++) {
 			
 			if(n % i==0) {
-//				System.out.println(i);
 				sum+=i;
 			}
 			
