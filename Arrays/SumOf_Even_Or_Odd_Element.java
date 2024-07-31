32030481b83e961a87bb001c9ca937176ab46365
Venktesh809 <103634213+Venktesh809@users.noreply.github.com>
2024-07-31 21:28:34 +0530

    Update SumOf_Even_Or_Odd_Element.java

--- Arrays/SumOf_Even_Or_Odd_Element.java
+++ Arrays/SumOf_Even_Or_Odd_Element.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class SumOf_Even_Or_Odd_Element {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		Scanner sc = new Scanner (System.in);
 		System.out.println("Enter the length of array");
 		int size = sc.nextInt();
