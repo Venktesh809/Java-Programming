c3671a4dcc94af4b8d351703b5cbf78126c632a5
Venktesh809 <103634213+Venktesh809@users.noreply.github.com>
2024-07-31 21:26:41 +0530

    Update Merge_2Arrays.java

--- Arrays/Merge_2Arrays.java
+++ Arrays/Merge_2Arrays.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Merge_2Arrays {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
 		Scanner sc = new Scanner (System.in);
 		System.out.println("Enter the length of 1st array");
 		int size = sc.nextInt();
