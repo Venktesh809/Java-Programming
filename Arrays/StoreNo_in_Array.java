708635fd0eca9bfe1a1056e57c6f83a610efce7f
Venktesh809 <103634213+Venktesh809@users.noreply.github.com>
2024-07-31 21:28:06 +0530

    Update StoreNo_in_Array.java

--- Arrays/StoreNo_in_Array.java
+++ Arrays/StoreNo_in_Array.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class StoreNo_in_Array {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the length of Array ");
 		int lenth = sc.nextInt();
