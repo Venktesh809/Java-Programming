460264b4eec1d003f7bdd836060c36f5d925abd7
Venktesh809 <103634213+Venktesh809@users.noreply.github.com>
2024-07-31 21:41:51 +0530

    Update Spy_No.java

--- numbers/Spy_No.java
+++ numbers/Spy_No.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Spy_No {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the no.");
 		int n = sc.nextInt();
